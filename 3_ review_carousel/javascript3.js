2aaf52075f35961b86e83f686cbb85e7feafcd3a
matejadas <matejadas@outlook.com>
2021-05-10 12:36:17 +0200

    Cambiado div class app por main tag, descartado swiper para proyecto 3

---
+++ 3_ review_carousel/javascript3.js
@@ -0,0 +1,12 @@
+/* Inicialización de swiper */
+const swiper = new Swiper(".swiper-container", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
