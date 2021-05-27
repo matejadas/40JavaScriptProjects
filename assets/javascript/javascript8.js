49acb3bdcf54b942d46dc818d29bea039748173e
matejadas <matejadas@outlook.com>
2021-05-27 14:54:34 +0200

    Proyecto 8 funcionalidad básica terminada

--- assets/javascript/javascript8.js
+++ assets/javascript/javascript8.js
@@ -1,142 +1,190 @@
-// Platos
+/* Modelo */
 const menu = [
     {
         id: 1,
         nombre: "Huevos fritos con chorizo",
         categoria: "platos combinados",
         precio: 3.50,
-        foto: "../assets/img/huevos_fritos.jpg",
+        ruta: "../assets/img/huevos_fritos.jpg",
         descripcion: "Un par de huevos fritos con chorizo y patatas fritas. Nunca falla.",
-        url: "http://cluberas.blogspot.com/2011/12/menus-en-crisis-2_15.html",
+        creditos: "http://cluberas.blogspot.com/2011/12/menus-en-crisis-2_15.html",
     },
     {
         id: 2,
         nombre: "Ensalada de lechuga",
         categoria: "platos combinados",
         precio: 2.50,
-        foto: "../assets/img/ensalada.jpg",
+        ruta: "../assets/img/ensalada.jpg",
         descripcion: "Ensalada de lechuga, zanahoria, tomate, berenjena y tofu.",
-        url: "https://cocinaconluzverde.blogspot.com/2010/06/ensalada-de-zanahorias-con-curry-y.html",
+        creditos: "https://cocinaconluzverde.blogspot.com/2010/06/ensalada-de-zanahorias-con-curry-y.html",
     },
     {
         id: 3,
         nombre: "Arroz a la cubana",
         categoria: "platos combinados",
         precio: 4.20,
-        foto: "../assets/img/arroz_cubana.ppg",
+        ruta: "../assets/img/arroz_cubana.png",
         descripcion: "Arroz con tomate, plátano frito y aguacate.",
-        url: "https://www.asopaipas.com/2018/10/arroz-la-cubana-video-receta.html",
+        creditos: "https://www.asopaipas.com/2018/10/arroz-la-cubana-video-receta.html",
     },
     {
         id: 4,
         nombre: "Fideos integrales con verduras asadas",
         categoria: "platos combinados",
         precio: 4.80,
-        foto: "../assets/img/verduras_asadas.jpg",
+        ruta: "../assets/img/verduras_asadas.jpg",
         descripcion: "Fideos integrales con patata, pimiento, tomate y zanahorias asadas.",
-        url: "https://www.pinterest.es/pin/317714948707010336/",
+        creditos: "https://www.pinterest.es/pin/317714948707010336/",
     },
     {
         id: 5,
         nombre: "Patatas bravas",
         categoria: "platos combinados",
         precio: 3.20,
-        foto: "../assets/img/patatas_bravas.jpg",
+        ruta: "../assets/img/patatas_bravas.jpg",
         descripcion: "Patatas bravas con... bueno, lo que sea eso.",
-        url: "http://descobrintgirona.blogspot.com/2013/10/Frankfurter-konig.html",
+        creditos: "http://descobrintgirona.blogspot.com/2013/10/Frankfurter-konig.html",
     },
     {
         id: 6,
         nombre: "Café con leche y croissant",
         categoria: "desayunos",
         precio: 1.80,
-        foto: "../assets/img/Cafe_con_leche_y_cruasan.jpg",
+        ruta: "../assets/img/Cafe_con_leche_y_cruasan.jpg",
         descripcion: "Desayuno rápido para empezar bien el día",
-        url: "https://es.wikipedia.org/wiki/Archivo:Cafe_con_leche_y_cruas%C3%A1n.jpg",
+        creditos: "https://es.wikipedia.org/wiki/Archivo:Cafe_con_leche_y_cruas%C3%A1n.jpg",
     },
     {
         id: 7,
         nombre: "Café y zumo de naranja",
         categoria: "desayunos",
         precio: 1.80,
-        foto: "../assets/img/desayuno_zumo.jpg",
+        ruta: "../assets/img/desayuno_zumo.jpg",
         descripcion: "Café, zumo de naranja y pan con jamón.",
-        url: "https://commons.wikimedia.org/wiki/File:Desayuno_en_Sevilla.jpg",
+        creditos: "https://commons.wikimedia.org/wiki/File:Desayuno_en_Sevilla.jpg",
     },
     {
         id: 8,
         nombre: "Café y batido",
         categoria: "desayunos",
         precio: 3.90,
-        foto: "../assets/img/desayuno_aguacate.jpg",
+        ruta: "../assets/img/desayuno_aguacate.jpg",
         descripcion: "Un poco de pan con hummus de aguacate, un huevo pasado por agua, café y un batido de kiwi y lima. No alimenta pero serás el más hipster del barrio.",
-        url: "https://www.elespanol.com/cocinillas/recetas/saludables/20140817/desayunos-tener-dia/1000002049813_30.html",
+        creditos: "https://www.elespanol.com/cocinillas/recetas/saludables/20140817/desayunos-tener-dia/1000002049813_30.html",
     },
     {
         id: 9,
         nombre: "Desayuno inglés",
         categoria: "desayunos",
         precio: 3.50,
-        foto: "../assets/img/desayuno_ingles.webp",
+        ruta: "../assets/img/desayuno_ingles.webp",
         descripcion: "Huevos a la plancha, judías, tomate y bacon. British total",
-        url: "https://pixabay.com/es/photos/desayuno-desayuno-ingles-completo-998220/",
+        creditos: "https://pixabay.com/es/photos/desayuno-desayuno-ingles-completo-998220/",
     },
     {
         id: 10,
         nombre: "Desayuno americano",
         categoria: "desayunos",
         precio: 2.80,
-        foto: "../assets/img/american_breakfast.jpg",
+        ruta: "../assets/img/american_breakfast.jpg",
         descripcion: "Huevos a la plancha, tomate y un gofre. No cuentes las calorías.",
-        url: "https://pixabay.com/es/photos/desayuno-desayuno-ingles-completo-998220/",
+        creditos: "https://pixabay.com/es/photos/desayuno-desayuno-ingles-completo-998220/",
     },
     {
         id: 11,
         nombre: "Batido de chocolate",
         categoria: "postres",
         precio: 1.50,
-        foto: "../assets/img/batido-chocolate.jpg",
+        ruta: "../assets/img/batido-chocolate.jpg",
         descripcion: "Batido de chocolate con plátanos, aguacate y dátiles.",
-        url: "https://www.cuerpomente.com/recetas-veganas/postres/postres-sanos-chocolate_2080",
+        creditos: "https://www.cuerpomente.com/recetas-veganas/postres/postres-sanos-chocolate_2080",
     },
     {
         id: 12,
         nombre: "Tarta de manzana",
         categoria: "postres",
         precio: 1.50,
-        foto: "../assets/img/tarta-de-manzana.jpg",
+        ruta: "../assets/img/tarta-de-manzana.jpg",
         descripcion: "Tarta de manzana, un clásico americano",
-        url: "https://postres-faciles.org/receta-de-postres-faciles/tarta-de-manzana-postres-faciles/",
+        creditos: "https://postres-faciles.org/receta-de-postres-faciles/tarta-de-manzana-postres-faciles/",
     },
     {
         id: 13,
         nombre: "Arroz con leche",
         categoria: "postres",
         precio: 1.00,
-        foto: "../assets/img/arroz-con-leche.jpg",
+        ruta: "../assets/img/arroz-con-leche.jpg",
         descripcion: "El clásico postre de la abuela, por si te habías quedado con hambre",
-        url: "https://buenavibra.es/casa/sabores/arroz-con-leche-la-receta-de-la-abuela-para-preparar-este-postre-de-fama-mundial/",
+        creditos: "https://buenavibra.es/casa/sabores/arroz-con-leche-la-receta-de-la-abuela-para-preparar-este-postre-de-fama-mundial/",
     },
     {
         id: 14,
         nombre: "Pastel de queso",
         categoria: "postres",
         precio: 1.20,
-        foto: "../assets/img/pastel_de_queso.jpg",
+        ruta: "../assets/img/pastel_de_queso.jpg",
         descripcion: "Pastel de queso, tan bueno como aparenta. ",
-        url: "https://familiaactiva.es/tarta-de-queso-con-thermomix/",
+        creditos: "https://familiaactiva.es/tarta-de-queso-con-thermomix/",
     },
     {
         id: 15,
         nombre: "Panacota",
         categoria: "postres",
         precio: 1.10,
-        foto: "../assets/img/panacota.jpg",
+        ruta: "../assets/img/panacota.jpg",
         descripcion: "Como el flan pero con base de nata y frutos rojos",
-        url: "https://www.cocinayvino.com/en-la-cocina/especiales/cinco-postres-italianos-famosos/",
+        creditos: "https://www.cocinayvino.com/en-la-cocina/especiales/cinco-postres-italianos-famosos/",
     },
-]
+];
+
+/* Controlador */
+const desayunos = menu.filter(item => item.categoria === "desayunos");
+const pCombinados = menu.filter(item => item.categoria === "platos combinados");
+const postres = menu.filter(item => item.categoria === "postres");
+
+/* Vista */
+function generaPlatos(arrayPlatos) {
+    const contPlatos = document.getElementById("platos");
+    contPlatos.innerHTML = "";
+
+    arrayPlatos.forEach(plato => {
+        let contenido =
+        `
+        <div class="plato">
+            <div class="foto">
+                <img src=${plato.ruta}>
+            </div>
+            <div class="descripcion">
+                <div class="encabezado">
+                    <div class="nombre">${plato.nombre}</div>
+                    <div class="precio">${plato.precio}</div>
+                </div>
+                <div class="texto">${plato.descripcion}</div>
+            </div>
+        </div>
+        `;
+
+        contPlatos.innerHTML += contenido;
+    });
+}
+
+/* Eventos */
+    /* Botones */
+let btnTodos = document.getElementById("todos");
+let btnDesayunos = document.getElementById("desayunos");
+let btnPCombinados = document.getElementById("pCombinados");
+let btnPostres = document.getElementById("postres");
+
+    /* Escuchadores */
 
 window.addEventListener("DOMContentLoaded", () => {
-    console.log("DOM cargado")
+    // Cargamos todos los platos
+    generaPlatos(menu);
+});
+
+document.addEventListener("click", (ev) => {
+    if (ev.target.matches("#todos")) generaPlatos(menu);
+    else if(ev.target.matches("#desayunos")) generaPlatos(desayunos);
+    else if(ev.target.matches("#pCombinados")) generaPlatos(pCombinados);
+    else if(ev.target.matches("#postres")) generaPlatos(postres);
 })
